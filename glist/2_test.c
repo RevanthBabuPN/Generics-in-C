e0eb300518f319140f675ec9cb3dd1eb92e578c4
revanthbabu <putturevanth@gmail.com>
2021-04-29 15:42:20 +0530

    add test files

---
+++ glist/2_test.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include "glist.h"
+
+typedef struct
+{
+	int dd;
+	int mm;
+	int yy;
+} date;
+
+int date_equal(date d1, date d2)
+{
+	return d1.dd == d2.dd && d1.mm == d2.mm && d1.yy == d2.yy;
+}
+
+int by_year(date d1, date d2)
+{
+	return d1.yy < d2.yy;
+}
+
+int by_month(date d1, date d2)
+{
+	return d1.mm < d2.mm;
+}
+
+glist(date, date_equal) datelist;
+
+int main()
+{
+	datelist l1 = gl_init(date);
+
+	date d1 = {11, 9, 2001};
+	date d2 = {2, 4, 2011};
+	date d3 = {26, 11, 2008};
+	date d4 = {30, 1, 1948};
+	date d5 = {26, 12, 2004};
+
+	gl_push_back(l1, d1);
+    gl_push_back(l1, d2);
+    gl_push_back(l1, d3);
+    gl_push_front(l1, d4);
+    gl_push_front(l1, d5);
+
+	iterator(date) it;
+	init_iterator(date, l1, &it);
+
+	printf("Before sorting:\n");
+	while(has_next_date(&it))
+	{
+		date temp = next_date(&it);
+		printf("%d - %d - %d\n", temp.dd, temp.mm, temp.yy);
+	}
+
+	gl_sort(l1, by_year);
+	printf("\n------------------------\nAfter sorting by year:\n");
+	iterator(date) it2;
+	init_iterator(date, l1, &it2);
+	while(has_next_date(&it2))
+	{
+		date temp = next_date(&it2);
+		printf("%d - %d - %d\n", temp.dd, temp.mm, temp.yy);
+	}
+
+	gl_sort(l1, by_month);
+	printf("\n------------------------\nAfter sorting by month:\n");
+	iterator(date) it3;
+	init_iterator(date, l1, &it3);
+	while(has_next_date(&it3))
+	{
+		date temp = next_date(&it3);
+		printf("%d - %d - %d\n", temp.dd, temp.mm, temp.yy);
+	}
+}
---
+++ glist/3_test.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "glist.h"
+
+typedef struct
+{
+	int rp;
+	int ip;
+} complex;
+
+int complex_equal(complex c1, complex c2)
+{
+	return c1.rp == c2.rp && c1.ip == c2.ip;
+}
+
+int by_rp(complex c1, complex c2)
+{
+	return c1.rp < c2.rp;
+}
+
+glist(complex, complex_equal) complexlist;
+
+int main()
+{
+	complexlist l1 = gl_init(complex);
+
+	complex c1 = {11, 9};
+	complex c2 = {2, 4};
+	complex c3 = {26, 11};
+	complex c4 = {30, 1};
+	complex c5 = {26, 12};
+
+	gl_push_back(l1, c1);
+    gl_push_back(l1, c2);
+    gl_push_back(l1, c3);
+    gl_push_front(l1, c4);
+    gl_push_front(l1, c5);
+
+	iterator(complex) it;
+	init_iterator(complex, l1, &it);
+
+	printf("Before sorting:\n");
+	while(has_next(complex, &it))
+	{
+		complex temp = next(complex, &it);
+		printf("%d : %d\n", temp.rp, temp.ip);
+	}
+
+	gl_sort(l1, by_rp);
+	printf("\n------------------------\nAfter sorting by rp:\n");
+	iterator(complex) it3;
+	init_iterator(complex, l1, &it3);
+	while(has_next(complex, &it3))
+	{
+		complex temp = next(complex, &it3);
+		printf("%d : %d\n", temp.rp, temp.ip);
+	}
+}
